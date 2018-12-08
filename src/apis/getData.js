4eae0fa0822f67a7a4867cdfffde22f3c2393ff6
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-08 20:41:02 +0100

    changed ip to allow for mobile in local wifi

--- src/apis/getData.js
+++ src/apis/getData.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const getData = async () => {
-    const response = await axios.get('http://127.0.0.1:5000/');
+    const response = await axios.get('http://192.168.188.24:5000/');
     const data = response.data;
     return data;
 }
