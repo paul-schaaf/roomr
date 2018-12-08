1704bf17a5fbaac0ebd3e932b28eccb135c15b32
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-08 17:50:27 +0100

    added roombox to Room.js, made it slide over time blocks during scroll

--- src/components/Room.js
+++ src/components/Room.js
@@ -5,9 +5,14 @@ const Room = props => {
 
   const renderedTimeBlocks = [];
   for (let i = 0; i < 32; i++) {
-    renderedTimeBlocks.push(<li className="room__timeblock"key={i}><div className="room__timeblock--block"></div></li>)
+    renderedTimeBlocks.push(<li className="room__timeblock-list__timeblock"key={i}><div className="room__timeblock-list__timeblock--block"></div></li>)
   }
-  return <ul className="room">{renderedTimeBlocks}</ul>
+  return (
+    <div className="room">
+      <div className="room__number-box">100</div>
+      <ul className="room__timeblock-list">{renderedTimeBlocks}</ul>
+    </div>
+  )
 }
 
 export default Room;
