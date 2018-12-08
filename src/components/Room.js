e81b0c3f665e9c4117633ebbe7bf161e4f172fb3
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-08 15:42:12 +0100

    created Room.js

---
+++ src/components/Room.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Room = props => {
+  return (
+    <div className="room">
+      <ul>
+        <li>09:00</li>
+        <li>09:15</li>
+        <li>09:30</li>
+        <li>09:45</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Room;
