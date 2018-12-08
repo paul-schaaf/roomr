1afed3c1dba88d8039fea639a84ba5896c43b4f3
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-08 15:56:16 +0100

    started rendering times for timeline

--- src/components/TimeLine.js
+++ src/components/TimeLine.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import './TimeLine.css';
 
 const TimeLine = props => {
-  return <div>hello there</div>;
+  const timeArray = [
+    "09:00",
+    "09:15",
+    "09:30",
+    "09:45",
+    "10:00",
+    "10:15",
+    "10:30",
+    "10:45",
+    "11:00",
+    "11:15",
+    "11:30",
+    "11:45",
+    "12:00",
+    "12:15",
+    "12:30",
+    "12:45",
+    "13:00",
+    "13:15",
+    "13:30",
+    "13:45",
+    "14:00",
+    "14:15",
+    "14:30",
+    "14:45",
+    "15:00",
+    "15:15",
+    "15:30",
+    "15:45",
+    "16:00",
+    "16:15",
+    "16:30",
+    "16:45",
+    "17:00"
+  ]
+  const renderedTimes = timeArray.map(time => {
+    return <li className="timeline__item">{time}</li>
+  })
+  return <ul className="timeline">{renderedTimes}</ul>;
 }
 
 export default TimeLine;
