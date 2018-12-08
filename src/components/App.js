10cda9c468cfa7b3088abc6d9a1b77c317cc7e7d
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-08 19:36:54 +0100

    created request logic and rendering depending on request result

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,44 @@ import React from 'react';
 import Form from './Form';
 import Room from './Room';
 import TimeLine from './TimeLine';
+import getData from '../apis/getData';
 import './App.css';
 class App extends React.Component {
+  state = {
+    status: "pending",
+    data: []
+  };
+
+  async componentDidMount() {
+    try {
+    const response = await getData();
+    const data = response.data;
+    this.setState({ data: data , status: "successful"});
+    } catch (err) {
+      console.error(err);
+      this.setState({ status: "failed"});
+    }
+  }
   render() {
-    return (
-      <React.Fragment>
-        <div className="form-area">
-          <Form />
-        </div>
-        <div className="info-area">
-            <TimeLine />
-          <div className="room-area">
-            <Room />
-            <Room />
+    if (this.state.status === "pending") {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <React.Fragment>
+          <div>{this.state.data[0].name}</div>
+          <div className="form-area">
+            <Form />
           </div>
-        </div>
-      </React.Fragment>
-    )
+          <div className="info-area">
+              <TimeLine />
+            <div className="room-area">
+              <Room />
+              <Room />
+            </div>
+          </div>
+        </React.Fragment>
+      )
+    }
   }
 }
 
