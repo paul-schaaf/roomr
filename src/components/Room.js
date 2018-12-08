b4d1d1b5afee1814e2cb34cbb1609117d1121541
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-08 17:02:05 +0100

    adjusted room.css so it aligns with the timeline

--- src/components/Room.js
+++ src/components/Room.js
@@ -5,7 +5,7 @@ const Room = props => {
 
   const renderedTimeBlocks = [];
   for (let i = 0; i < 32; i++) {
-    renderedTimeBlocks.push(<li className="room__timeblock"key={i}><div className="room__timeblock--block">vacant</div></li>)
+    renderedTimeBlocks.push(<li className="room__timeblock"key={i}><div className="room__timeblock--block"></div></li>)
   }
   console.log(renderedTimeBlocks)
 
