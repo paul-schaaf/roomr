86c6d0577d171fa003beeff3ddf543dbc9f17661
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-08 19:36:32 +0100

    moved error handling into App.js

--- src/apis/getData.js
+++ src/apis/getData.js
@@ -1,12 +1,8 @@
 import axios from 'axios';
 
 const getData = async () => {
-  try {
   const response = await axios.get('http://127.0.0.1:5000/');
   return response;
-  } catch (err) {
-    return err.message;
-  }
 }
 
 export default getData;
