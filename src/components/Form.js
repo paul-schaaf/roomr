ce4ffa4926d190532c078a3f51643967b5c7edb5
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-08 12:54:01 +0100

    added change handler to input elements in Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,15 +4,23 @@ import './Form.css';
 
 
 class Form extends React.Component {
+  state = {
+    room: '',
+    start: '',
+    end: ''
+  };
 
+  onInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value})
+  }
 
   render () {
     return (
       <div className="form">
         <form>
-          <input type="text"></input>
-          <input type="text"></input>
-          <input type="text"></input>
+          <input name="room" onChange={this.onInputChange} value={this.state.room} type="text"></input>
+          <input name="start" onChange={this.onInputChange} value={this.state.start} type="text"></input>
+          <input name="end" onChange={this.onInputChange} value={this.state.end} type="text"></input>
           <input type="submit"></input>
         </form>
       </div>
