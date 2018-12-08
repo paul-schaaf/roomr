9de32e89d43cb9954d7f48fb574ef29bcb43244a
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-08 13:51:22 +0100

    created InputTimes component and start rendering dynamic times

---
+++ src/components/InputTimes.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const InputTimes = props => {
+  const timeArray = [
+    "09:00",
+    "09:15",
+    "09:30",
+    "09:45",
+    "10:00",
+    "10:15",
+    "10:30",
+    "10:45",
+    "11:00",
+    "11:15",
+    "11:30",
+    "11:45",
+    "12:00",
+    "12:15",
+    "12:30",
+    "12:45",
+    "13:00",
+    "13:15",
+    "13:30",
+    "13:45",
+    "14:00",
+    "14:15",
+    "14:30",
+    "14:45",
+    "15:00",
+    "15:15",
+    "15:30",
+    "15:45",
+    "16:00",
+    "16:15",
+    "16:30",
+    "16:45",
+    "17:00"
+  ]
+  if (!props.start) {
+    let renderedTimes = timeArray.map(time => {
+      return <option value={time}>{time}</option>
+    })
+    renderedTimes.pop();
+    return <React.Fragment>{renderedTimes}</React.Fragment>
+  }
+
+  
+  
+  return <option value={timeArray[0]}>{timeArray[0]}</option>
+}
+
+export default InputTimes;
