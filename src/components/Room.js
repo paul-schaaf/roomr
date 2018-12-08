f155c7040f66c82ef12a38b10392d3b1b33c9ac9
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-08 20:32:31 +0100

    render and style Rooms according to data passed from RoomList and App

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './Room.css';
 
-const Room = props => {
-
-  const renderedTimeBlocks = [];
-  for (let i = 0; i < 32; i++) {
-    renderedTimeBlocks.push(<li className="room__timeblock-list__timeblock"key={i}><div className="room__timeblock-list__timeblock--block"></div></li>)
+const Room = ({ roomData }) => {
+  const config = {
+    "true": "green",
+    "false": "red"
   }
+  
+  const renderedTimeBlocks = roomData.times.map(timeSpanObject => {
+    const timespan = Object.keys(timeSpanObject)[0];
+    const availability = Object.values(timeSpanObject)[0];
+    return (
+      <li className="room__timeblock-list__timeblock" key={timespan}>
+        <div className={`room__timeblock-list__timeblock--block ${config[availability]}`}></div>
+      </li>
+    )
+  })
+
   return (
     <div className="room">
-      <div className="room__number-box">1000</div>
+      <div className="room__number-box">{roomData.name}</div>
       <ul className="room__timeblock-list">{renderedTimeBlocks}</ul>
     </div>
   )
