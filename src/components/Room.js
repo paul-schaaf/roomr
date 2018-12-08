eb65ab7a04f120ad0ac22723b759baa162031d07
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-08 16:54:02 +0100

    made room render 32 (required number of timeblocks) timeblocks

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 const Room = props => {
-  return (
-    <div className="room">
-      <ul>
-        <li>vacant</li>
-        <li>vacant</li>
-        <li>full</li>
-        <li>full</li>
-      </ul>
-    </div>
-  );
+
+  const renderedTimeBlocks = [];
+  for (let i = 0; i < 32; i++) {
+    renderedTimeBlocks.push(<li key={i}>vacant</li>)
+  }
+  console.log(renderedTimeBlocks)
+
+  return <ul className="room">{renderedTimeBlocks}</ul>
 }
 
 export default Room;
