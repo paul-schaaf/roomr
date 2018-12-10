1f6f8ab8c869317db4b16e96053401bf570cb7a1
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-10 21:16:43 +0100

    moved routing logic into routes.js

--- app.js
+++ app.js
@@ -1,34 +1,11 @@
 const express = require("express");
-const fs = require("fs");
 const cors = require("cors");
+const routes = require('./routes/routes');
 
 const app = express();
 
 app.use(cors());
 
-const getFileContents = () => new Promise((resolve, reject) => {
-  let data = '';
-  const response = fs.createReadStream('./rooms.json');
-  response.on('error', err => reject(err));
-  response.on('data', (chunk) => {
-    data += chunk.toString();
-  });
-  response.on('end', () => resolve(data));
-});
-
-
-
-app.get('/', async (req, res) => {
-  try {
-    const data = await getFileContents();
-    res.writeHead(200, {"Content-Type": "application/json"})
-    res.end(data);
-  } catch(err) {
-    res.writeHead(404);
-    res.end(err);
-  }
-
-})
-
+routes(app);
 
 app.listen(5000);
---
+++ routes/routes.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+module.exports = (app) => {
+  const getFileContents = () => new Promise((resolve, reject) => {
+    let data = '';
+    const response = fs.createReadStream('./rooms.json');
+    response.on('error', err => reject(err));
+    response.on('data', (chunk) => {
+      data += chunk.toString();
+    });
+    response.on('end', () => resolve(data));
+  });
+  
+  
+  
+  app.get('/', async (req, res) => {
+    try {
+      const data = await getFileContents();
+      res.writeHead(200, {"Content-Type": "application/json"})
+      res.end(data);
+    } catch(err) {
+      res.writeHead(404);
+      res.end(err);
+    }
+  })
+};
