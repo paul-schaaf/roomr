b668349ec63b4139cc882f4a6d984b736384a5ae
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-10 22:07:05 +0100

    connected mlab database to app

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes/routes');
-
 const app = express();
+
+mongoose.connect('mongodb://paul:ha95W4iYzUYVP8b@ds039175.mlab.com:39175/roomr-dev', { useNewUrlParser:true }).catch((err) => console.log(err.message));
 
 app.use(bodyParser.json());
 app.use(cors());
