31ac28fb546a61aa392fa681abe9609aab869988
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-10 21:29:47 +0100

    cleaned up typo

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const post = require('./roomSubSchema');
+const room = require('./roomSubSchema');
 
 const userSchema = new Schema ({
   email: String,
