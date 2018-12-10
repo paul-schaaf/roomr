581de6790ce20ec8fd720eaaabb9ece2615aaba9
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-10 16:07:24 +0100

    added "add room" form to Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,7 +8,8 @@ class Form extends React.Component {
   state = {
     room: '',
     start: '09:00',
-    end: '09:15'
+    end: '09:15',
+    addedRoom: ''
   };
 
   onInputChange = (event) => {
@@ -19,19 +20,23 @@ class Form extends React.Component {
 
   render () {
     return (
-      <div className="form">
-        <form>
-          <input autoComplete="off" name="room" list="rooms" onChange={this.onInputChange} value={this.state.room} type="text" placeholder="room (optional)"></input>
-          <DataList data={this.props.data}/>
-          <select name="start" onChange={this.onInputChange} value={this.state.start} type="text">
-            <InputTimes />
-          </select>
-          <select name="end" onChange={this.onInputChange} value={this.state.end} type="text">
-            <InputTimes start={this.state.start}/>
-          </select>
-          <input type="submit"></input>
-        </form>
-      </div>
+        <div className="forms">
+          <form>
+            <input autoComplete="off" name="room" list="rooms" onChange={this.onInputChange} value={this.state.room} type="text" placeholder="room... (optional)"></input>
+            <DataList list="rooms" data={this.props.data}/>
+            <select name="start" onChange={this.onInputChange} value={this.state.start} type="text">
+              <InputTimes />
+            </select>
+            <select name="end" onChange={this.onInputChange} value={this.state.end} type="text">
+              <InputTimes start={this.state.start}/>
+            </select>
+            <input type="submit" value="find room"></input>
+          </form>
+          <form>
+            <input name="addedRoom" value={this.state.addedRoom} onChange={this.onInputChange} type="text" placeholder="room..."></input>
+            <input type="submit" value="add room"></input>
+          </form>
+        </div>
     )
   }
 }
