de7448c5c744075eca6312a5fa705298f2cf3f1b
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-10 21:44:31 +0100

    added body parser for post requests

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes/routes');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(cors());
 
 routes(app);
