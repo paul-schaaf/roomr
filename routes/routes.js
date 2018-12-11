d96b9e01a9a6a438e39d2c049ac5eed88a42338b
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-11 20:35:58 +0100

    edit get route

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const UsersController = require('../controllers/users_controller');
 
 module.exports = (app) => {
   
-  app.get('/', UsersController.getAllRooms);
+  app.get('/api/users/rooms', UsersController.getAllRooms);
 
   app.post('/api/users/rooms', UsersController.createRoom);
 
