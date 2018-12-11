0e463e020a686b9eab7274429fba9f5a5178493e
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-11 13:23:48 +0100

    added some todos in routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -5,5 +5,17 @@ module.exports = (app) => {
   app.get('/', UsersController.getAllRooms);
 
   app.post('/api/users/rooms', UsersController.createRoom);
+
+  //TODO: implement deletion function, implement block time function
+
+  /**
+  * future function to delete room from app
+  * app.delete('/api/users/rooms', UsersController.deleteRoom);
+  **/
+
+  /**
+  * future function to block time in room in app
+  * app.put('/api/users/rooms/times', UsersController.editTime);
+  **/
   
 };
