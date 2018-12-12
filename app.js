bb5ce8b2ad3dd5d715e00a99bb4b3b3ed6af1653
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-12 18:15:05 +0100

    did some reordering

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ const routes = require('./routes/routes');
 
 const port = process.env.PORT || 5000;
 
-const app = express();
-
 mongoose.connect('mongodb://paul:ha95W4iYzUYVP8b@ds039175.mlab.com:39175/roomr-dev', { useNewUrlParser:true }).catch((err) => console.log(err.message));
+
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
 
 routes(app);
 
