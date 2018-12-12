1b42727daba90f4420e8a2e58a49fe8f3619120e
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-12 18:08:49 +0100

    added environmental port or port 5000 for server to listen on

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes/routes');
+
+const port = process.env.PORT || 5000;
+
 const app = express();
 
 mongoose.connect('mongodb://paul:ha95W4iYzUYVP8b@ds039175.mlab.com:39175/roomr-dev', { useNewUrlParser:true }).catch((err) => console.log(err.message));
