2768f0e2c9e3eec8f6a716ade3bc9929644672cb
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-15 15:51:59 +0100

    axiosErrorHandler commenting

--- src/errorHandling/axiosErrorHandler.js
+++ src/errorHandling/axiosErrorHandler.js
@@ -2,11 +2,12 @@ const axiosErrorHandler = async (fn, appState, reqData) => {
   try {
     await fn(appState, reqData);
   } catch(err) {
+    //this error appears when there is an error from the client (e.g. room already exists);
     if(err.response) {
       appState.setState({"errorMessage":err.response.data});
-    } else if (err.request) {
+    } else if (err.request) {                 // this error appears when there is no response from the server
       appState.setState({ getStatus: "failed"});
-    } else {
+    } else {                      // this error appears when a custom error is created in roomrapi.js before axios runs
       appState.setState({"errorMessage":err.message});
     }
   }
@@ -14,3 +15,11 @@ const axiosErrorHandler = async (fn, appState, reqData) => {
 
 
 export default axiosErrorHandler;
+
+/*
+* axiosErrorHandler wraps whatever function given to it and executes it within a try-catch block
+* it also requires "appState" so it can change the state in App.js
+* finally it nmay need reqData which is the form data used to make a request to the server
+* roomrapi functions, passed into the error Handler get the reqData to use with axios
+* and also the appState for state changes that depend on the roomrapi function being used
+*/
