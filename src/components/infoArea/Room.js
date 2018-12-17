35d4b4f12e9728b89379a0fd8683dd8b9210ce07
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-17 13:34:27 +0100

    commented Room.js

--- src/components/infoArea/Room.js
+++ src/components/infoArea/Room.js
@@ -6,7 +6,12 @@ const Room = ({ roomData }) => {
     "true": "green",
     "false": "red"
   }
-  
+  /*
+  * every room in the database has a 'times' array with objects that hold a timespan and the corresponding availability
+  * of the room, e.g. "09:15" and true. We loop over the times array to create an li element for each timespan.
+  * we use the availability property to give the div inside the li a class of either green or red, making the html block
+  * green (or in the current color theme: blue) and red
+  */
   const renderedTimeBlocks = roomData.times.map(timeSpanObject => {
     const timespan = timeSpanObject.time.default;
     const availability = timeSpanObject.availability.toString();
