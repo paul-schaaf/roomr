1e2a2acdf5b6aa54837298979020046c0eef4bd5
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-17 13:36:56 +0100

    RoomList comments done

--- src/components/infoArea/RoomList.js
+++ src/components/infoArea/RoomList.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Room from './Room';
 
+/*
+* state.data that is passed down into this component holds the array of rooms in our database
+* we loop over the array to create and render rooms
+*/
+
 const RoomList = ({ data }) => {
   const renderedRooms = data.map((room) => {
     return <Room key={room.roomName} roomData={room}/>
