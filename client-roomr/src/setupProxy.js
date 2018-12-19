10602d90a52bd379f07cdde0194b23c3f754cfd3
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-19 18:28:42 +0100

    previous change causes crash

--- client-roomr/src/setupProxy.js
+++ client-roomr/src/setupProxy.js
@@ -1,5 +1,5 @@
-import proxy from 'http-proxy-middleware';
+const proxy = require('http-proxy-middleware')
  
-export default function(app) {
+module.exports = function(app) {
     app.use(proxy('/api', { target: 'http://localhost:5000' }))
 }
