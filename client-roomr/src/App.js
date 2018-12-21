f549f1043c0fc9f1204f2401b41f5e4149cce761
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-21 22:27:34 +0100

    added login page to app - works too!

--- client-roomr/src/App.js
+++ client-roomr/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import BookingPage from './components/bookingPage/BookingPage';
 import AdminPage from './components/adminPage/AdminPage';
+import LoginPage from './components/loginPage/LoginPage';
 
 class App extends React.Component {
   render () {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <React.Fragment>
           <Route path="/" exact component={BookingPage} />
           <Route path="/admin" exact component={AdminPage} />
+          <Route path="/login" exact component={LoginPage} />
         </React.Fragment>
       </Router>
     );
---
+++ client-roomr/src/components/loginPage/LoginPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class LoginPage extends React.Component {
+  render () {
+    return <div>This is the login page.</div>
+  }
+}
+
+export default LoginPage;
