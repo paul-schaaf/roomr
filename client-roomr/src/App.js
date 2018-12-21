77b4edb94b1a968d706fa471d6573cc9ef43dacd
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-21 22:24:53 +0100

    added adminpage to test react router - it works!

--- client-roomr/src/App.js
+++ client-roomr/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import BookingPage from './components/bookingPage/BookingPage';
+import AdminPage from './components/adminPage/AdminPage';
 
 class App extends React.Component {
   render () {
     return (
       <Router>
-        <Route path="/" exact component={BookingPage} />
-        <BookingPage />
+        <React.Fragment>
+          <Route path="/" exact component={BookingPage} />
+          <Route path="/admin" exact component={AdminPage} />
+        </React.Fragment>
       </Router>
     );
   }
---
+++ client-roomr/src/components/adminPage/AdminPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class AdminPage extends React.Component {
+  render () {
+    return <div>This is the admin page</div>
+  }
+}
+
+export default AdminPage;
