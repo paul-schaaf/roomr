fd2863e51a63aa233956cc7f6e506b142e902c02
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-22 11:41:29 +0100

    dynamic styling for login box

--- client-roomr/src/components/loginPage/LoginPage.js
+++ client-roomr/src/components/loginPage/LoginPage.js
@@ -2,20 +2,64 @@ import React from 'react';
 import './LoginPage.css';
 
 class LoginPage extends React.Component {
+  state = {
+    tab: 'login' ,
+    loginColor: '#1E4363',
+    createColor: '#152F45',
+    submitValue: 'login',
+    entityValue: '',
+    emailValue: '',
+    passwordValue: ''
+  };
+
+  onLoginTab = () => {
+    this.setState({ tab: 'login', loginColor: '#1E4363', createColor: '#152F45' });
+  }
+
+  onCreateTab = () => {
+    this.setState({ tab: 'create', loginColor: '#152F45', createColor: '#1E4363' });
+  }
+
+  onInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
   render () {
     return (
       <div className="login-page">
         <div className="login-box">
           <div className="login-box__tabs">
-            <div className="login-box__tabs__login">Login</div>
-            <div className="login-box__tabs__create">Create</div>
+            <div style={{ background: this.state.loginColor }} onClick={this.onLoginTab} >Login</div>
+            <div style={{ background: this.state.createColor }} onClick={this.onCreateTab} >Create</div>
           </div>
           <div className="login-box__form-box">
             <form>
-              <input className="login-box__form-box__input" type="text" placeholder="entity..."></input>
-              <input className="login-box__form-box__input"type="text" placeholder="email..."></input>
-              <input className="login-box__form-box__input"type="text" placeholder="password..."></input>
-              <input className="login-submit" type="submit" value="login"></input>
+              <input
+                name="entityValue"
+                onChange={this.onInputChange}
+                className="login-box__form-box__input"
+                type="text"
+                placeholder="entity...">
+              </input>
+              <input
+                name="emailValue"
+                onChange={this.onInputChange}
+                className="login-box__form-box__input"
+                type="text"
+                placeholder="email...">
+              </input>
+              <input
+                name="passwordValue"
+                onChange={this.onInputChange}
+                className="login-box__form-box__input"
+                type="password"
+                placeholder="password...">
+              </input>
+              <input
+                className="login-submit"
+                type="submit"
+                value={this.state.tab}>
+              </input>
             </form>
           </div>
         </div>
