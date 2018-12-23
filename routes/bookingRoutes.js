c50a4aaf5d01f02c1fb1dff081fa27d73e20eee7
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-24 00:06:15 +0100

    booking routes for now without requirelogin to make develpoment easier, also added "entities" to urls

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -2,15 +2,17 @@ const UsersController = require('../controllers/users_controller');
 const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-  app.delete('/api/users/rooms/:id', requireLogin,UsersController.deleteRoom);
+  app.delete('/api/entities/rooms/:entity/:roomName', UsersController.deleteRoom);
 
-  app.get('/api/users/rooms', requireLogin, UsersController.getAllRooms);
+  app.get('/api/entities/rooms/:entity', UsersController.getAllRooms);
 
-  app.post('/api/users/rooms', requireLogin, UsersController.createRoom);
+  app.post('/api/entities', UsersController.createEntity);
 
-  app.post('/api/users', requireLogin, UsersController.createUser);
+  app.post('/api/entities/rooms', UsersController.createRoom);
 
-  app.post('/api/users/rooms/times-block', requireLogin, UsersController.blockRoom);
+  app.post('/api/entities/users', UsersController.createUser);
 
-  app.post('/api/users/rooms/times-unblock', requireLogin, UsersController.unblockRoom);
+  app.post('/api/entities/rooms/times-block', UsersController.blockRoom);
+
+  app.post('/api/entities/rooms/times-unblock', UsersController.unblockRoom);
 };
