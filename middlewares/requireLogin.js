73f22222e3a2263f52ee193c48302a0f89025dc0
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-24 00:00:30 +0100

    added comments to clarify purpose

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,5 +1,9 @@
 const requireLogin = (req, res, next) => {
   try {
+    /**
+     * checks if user is logged in, if user is loggin in, passport - which
+     * runs before requireLogin - should have added a "user" property to req
+     */
     if(!req.user) {
       res.locals.type = 'clientErrorUnauthorized';
       throw new Error('You are not authorized to access this page. Please log in first.')
