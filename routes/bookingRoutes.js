b9c610525992c517d7331dbdb199e32ad946b7b8
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-24 13:27:43 +0100

    removed unnecessary /

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,7 +4,7 @@ const requireLogin = require('../middlewares/requireLogin');
 module.exports = app => {
   app.delete('/api/entities/rooms/:roomName', UsersController.deleteRoom);
 
-  app.get('/api/entities/rooms/', requireLogin, UsersController.getAllRooms);
+  app.get('/api/entities/rooms', requireLogin, UsersController.getAllRooms);
 
   app.post('/api/entities', requireLogin, UsersController.createEntity);
 
