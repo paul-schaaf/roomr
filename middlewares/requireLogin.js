7b28bcd2f102953d9d2f9bdf05af4423bb72bee3
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-24 01:08:52 +0100

    added entityCheck for members who try to access other members info

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -8,6 +8,18 @@ const requireLogin = (req, res, next) => {
       res.locals.type = 'clientErrorUnauthorized';
       throw new Error('You are not authorized to access this page. Please log in first.')
     }
+
+    /**
+     * The previous check checks only whether a user is logged in. Once a user is logged in, he could try accessing
+     * other members' info
+     * This check makes sure that cannot happen by comparing the activeEntity aka the one he used to login with the entity
+     * he is trying to access now
+     *
+     */
+    if(req.user.activeEntity !== req.body.entity) {
+      res.locals.type = 'clientErrorUnauthorized';
+      throw new Error('You are trying to authorize an entity that you are currently not logged into.')
+    }
     next()
   } catch(err) {
     next(err);
