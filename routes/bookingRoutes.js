da693b3d088fef42287bac855edd11e572f2286c
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-24 13:27:13 +0100

    corrected routes for usage of req.user.activeEntity

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -2,17 +2,17 @@ const UsersController = require('../controllers/users_controller');
 const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-  app.delete('/api/entities/rooms/:entity/:roomName', UsersController.deleteRoom);
+  app.delete('/api/entities/rooms/:roomName', UsersController.deleteRoom);
 
   app.get('/api/entities/rooms/', requireLogin, UsersController.getAllRooms);
 
-  app.post('/api/entities', UsersController.createEntity);
+  app.post('/api/entities', requireLogin, UsersController.createEntity);
 
-  app.post('/api/entities/rooms', UsersController.createRoom);
+  app.post('/api/entities/rooms', requireLogin, UsersController.createRoom);
 
-  app.post('/api/entities/users', UsersController.createUser);
+  app.post('/api/entities/users', requireLogin, UsersController.createUser);
 
-  app.post('/api/entities/rooms/times-block', UsersController.blockRoom);
+  app.post('/api/entities/rooms/times-block', requireLogin, UsersController.blockRoom);
 
-  app.post('/api/entities/rooms/times-unblock', UsersController.unblockRoom);
+  app.post('/api/entities/rooms/times-unblock', requireLogin, UsersController.unblockRoom);
 };
