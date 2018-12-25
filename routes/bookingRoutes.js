844acdcadd0da42f886744a42727e2c040fbd684
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-25 13:14:25 +0100

    adding and deleting rooms are now Admin priviliges

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,16 +1,17 @@
 const entitiesController = require('../controllers/entities_controller');
 const requireLogin = require('../middlewares/requireLogin');
+const requireAdmin = require('../middlewares/requireAdmin');
 
 module.exports = app => {
-  app.delete('/api/entities/rooms/:roomName', requireLogin, entitiesController.deleteRoom);
+  app.delete('/api/entities/rooms/:roomName', requireLogin, requireAdmin, entitiesController.deleteRoom);
 
   app.get('/api/entities/rooms', requireLogin, entitiesController.getAllRooms);
 
   app.post('/api/entities', entitiesController.createEntity);
 
-  app.post('/api/entities/rooms', requireLogin, entitiesController.createRoom);
+  app.post('/api/entities/rooms', requireLogin, requireAdmin, entitiesController.createRoom);
 
-  app.post('/api/entities/users', requireLogin, entitiesController.createUser);
+  app.post('/api/entities/users', entitiesController.createUser);
 
   app.post('/api/entities/rooms/times-block', requireLogin, entitiesController.blockRoom);
 
