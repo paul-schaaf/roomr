3f327806410a7b5103633e264cc40c1e8a2b2a61
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-25 13:26:22 +0100

    removed unneccessary mongoose model connection

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,6 @@
 const passport = require('passport');
 const mongoose = require('mongoose');
 
-const User = mongoose.model('users');
-
 //logs in user, then redirects to /'entityName'
 module.exports = app => {
   app.post('/api/login',
