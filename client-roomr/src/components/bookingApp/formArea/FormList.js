7bba710636d92d49f414d754018cbd157c46217b
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-25 21:12:58 +0100

    more css to make forms look good on mobile

--- client-roomr/src/components/bookingApp/formArea/FormList.js
+++ client-roomr/src/components/bookingApp/formArea/FormList.js
@@ -15,32 +15,36 @@ import './FormList.css';
 const FormList = props =>  {
     return (
         <div className="forms">
-          <Form 
-            data={props.data}
-            formAction={props.onBlockRoomSubmit}
-            buttonValue="▹ reserve room"
-            datalistName="reserve"
-          />
-          <Form
-            data={props.data}
-            formAction={props.onUnblockRoomSubmit}
-            buttonValue="▹ unblock room"
-            datalistName="unblock"
-          />
-          {props.isAdmin && < Form
-            data={props.data}
-            formAction={props.onAddRoomSubmit}
-            buttonValue="▹ add room"
-            DataListRequired="false"
-            noSelect="true"
-          />}
-          {props.isAdmin && <Form
-            data={props.data}
-            formAction={props.onDeleteRoomSubmit}
-            buttonValue="▹ delete room" 
-            noSelect="true"
-            datalistName="delete"
-          />}
+          <div className="forms--upper">
+            <Form 
+              data={props.data}
+              formAction={props.onBlockRoomSubmit}
+              buttonValue="▹ reserve room"
+              datalistName="reserve"
+            />
+            <Form
+              data={props.data}
+              formAction={props.onUnblockRoomSubmit}
+              buttonValue="▹ unblock room"
+              datalistName="unblock"
+            />
+          </div>
+          <div className="forms--lower">
+            {props.isAdmin && < Form
+              data={props.data}
+              formAction={props.onAddRoomSubmit}
+              buttonValue="▹ add room"
+              DataListRequired="false"
+              noSelect="true"
+            />}
+            {props.isAdmin && <Form
+              data={props.data}
+              formAction={props.onDeleteRoomSubmit}
+              buttonValue="▹ delete room" 
+              noSelect="true"
+              datalistName="delete"
+            />}
+          </div>
         </div>
     )
 }
