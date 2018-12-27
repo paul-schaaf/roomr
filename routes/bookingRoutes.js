378f9383bd98e97bb17506404526c2814720ff3c
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-27 19:51:54 +0100

    added route for deleteUser

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,15 +3,17 @@ const requireLogin = require('../middlewares/requireLogin');
 const requireAdmin = require('../middlewares/requireAdmin');
 
 module.exports = app => {
+  app.get('/api/entities/rooms', requireLogin, entitiesController.getAllRooms);
+
   app.delete('/api/entities/rooms/:roomName', requireLogin, requireAdmin, entitiesController.deleteRoom);
 
-  app.get('/api/entities/rooms', requireLogin, entitiesController.getAllRooms);
+  app.delete('/api/entities/users/:user', requireLogin, requireAdmin, entitiesController.deleteUser);
 
   app.post('/api/entities', entitiesController.createEntity);
 
-  app.post('/api/entities/rooms', requireLogin, requireAdmin, entitiesController.createRoom);
-
   app.post('/api/entities/users', requireLogin, requireAdmin, entitiesController.createUser);
+
+  app.post('/api/entities/rooms', requireLogin, requireAdmin, entitiesController.createRoom);
 
   app.post('/api/entities/rooms/times-block', requireLogin, entitiesController.blockRoom);
 
