dd18f0b579538c52f96f5e202f830ba1398d10dc
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-27 15:56:27 +0100

    made bookingApp slimmer by using conditionals-in-jsx

--- client-roomr/src/components/bookingApp/BookingApp.js
+++ client-roomr/src/components/bookingApp/BookingApp.js
@@ -11,8 +11,8 @@ import SettingsButton from './SettingsButton';
 import './BookingApp.css';
 
 /**
-* this component is responsible for rendering the app
-* state.getStatus saves whether the get requests for the data in roomrapi.js were successful. If not App.js
+* this component is responsible for rendering the booking app
+* state.getStatus saves whether the get requests for the data in roomrapi.js were successful. If not BookingApp.js
 * renders the ErrorPage.js
 *
 * state.errorType shows the type of error in a shorter format
@@ -20,10 +20,10 @@ import './BookingApp.css';
 * state.responseMessage saves whether all other requests(post, delete) were successful and renders dynamic error messages
 * depending for example on the server response using ResponseMessage.js
 *
-* state.data holds all data received through the get requests and App.js passes that data down to the individual components
+* state.data holds all data received through the get requests and BookingApp.js passes that data down to the individual components
 * in the case where
 *
-* in the case that the get request was successful but the database is empty App.js will only render the FormList component but not
+* in the case that the get request was successful but the database is empty BookingApp.js will only render the FormList component but not
 * the RoomList component
 */
 class BookingPage extends React.Component {
@@ -105,7 +105,7 @@ class BookingPage extends React.Component {
     }
     
     //request was successful and there is data in the database
-    if (this.state.getStatus === "successful" && this.state.data.length > 0){
+    if (this.state.getStatus === "successful"){
       return (
         <React.Fragment>
           {this.state.errorType === 'clientError' && <ResponseMessage clientError responseMessage={this.state.responseMessage} onMessageButtonClick={this.onMessageButtonClick}/>}
@@ -128,40 +128,19 @@ class BookingPage extends React.Component {
               onDeleteUserSubmit={this.onDeleteUserSubmit}
             />
           </div>
-          <div className="info-area">
+           {/**
+            * only show info-area if there are rooms in the database
+            */}
+          {this.state.data.length > 0 && <div className="info-area">
               <TimeLine />
             <div className="room-area">
               <RoomList data={this.state.data}/>
               
             </div>
-          </div>
+          </div>}
         </React.Fragment>
       )
     } 
-    //request successful but database still empty
-    return (
-      <React.Fragment>
-        {this.state.errorType === 'clientError' && <ResponseMessage responseMessage={this.state.responseMessage} onMessageButtonClick={this.onMessageButtonClick}/>}
-        <div className="form-area">
-          <LogoutButton />
-          {this.props.match.params.isAdmin &&
-              <SettingsButton
-                onSettingsButtonClick={this.onSettingsButtonClick}
-                showSettings={this.state.showSettings}
-              />}
-          <FormList
-            showSettings={this.state.showSettings}
-            data={this.state.data}
-            onAddRoomSubmit={this.onAddRoomSubmit} 
-            onDeleteRoomSubmit={this.onDeleteRoomSubmit}
-            onBlockRoomSubmit={this.onBlockRoomSubmit}
-            onUnblockRoomSubmit={this.onUnblockRoomSubmit}
-            onAddUserSubmit={this.onAddUserSubmit}
-            onDeleteUserSubmit={this.onDeleteUserSubmit}
-          />
-        </div>
-      </React.Fragment>
-    )
   }
 }
 
