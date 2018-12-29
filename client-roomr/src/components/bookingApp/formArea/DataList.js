4559566d853f67053a19bcc8676cae81abb293ba
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-30 00:15:28 +0100

    Datalist can now render rooms or users

--- client-roomr/src/components/bookingApp/formArea/DataList.js
+++ client-roomr/src/components/bookingApp/formArea/DataList.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const DataList = ({ data, id }) => {
-  const renderedOptions = data.map(roomObject => {
-    return <option key={roomObject.roomName}value={roomObject.roomName}></option>
-  })
+const DataList = ({ data, id, type }) => {
+  if(type === 'room') {
+    const renderedOptions = data.map(roomObject => {
+      return <option key={roomObject.roomName}value={roomObject.roomName}></option>
+    })
+  
+    return <datalist id={id}>{renderedOptions}</datalist>;
+  }
+  
+  if(type === 'user') {
+    const renderedUsers = data.map((userObject) => {
+      return <option key={userObject} value={userObject}></option>
+    })
 
-  return <datalist id={id}>{renderedOptions}</datalist>;
+    return <datalist id={id}>{renderedUsers}</datalist>;
+  }
 }
 
 export default DataList;
