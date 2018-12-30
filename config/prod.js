8a1593b6dd99ecc01265bcb6f3fa5d8070fccc2e
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-30 13:49:48 +0100

    added production and dev CookieKeys to keys config file

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 const keys = {
   mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
 };
 
 module.exports = keys;
--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: ['jsgfsdjgsgaasogrgvertiwognwkegew']
+    keys: [keys.cookieKey]
   })
 )
 
