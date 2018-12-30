ea2b9fd32354caf081f3b638654e83397be65640
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-30 17:50:59 +0100

    linted App.js

--- client-roomr/src/App.js
+++ client-roomr/src/App.js
@@ -3,21 +3,17 @@ import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import BookingApp from './components/bookingApp/BookingApp';
 import LoginPage from './components/loginPage/LoginPage';
 
-class App extends React.Component {
-  render () {
-    return (
-      <Router>
-        <React.Fragment>
-        <Route path="/app/:id/:isAdmin" exact component={BookingApp} />
-        <Route path="/login" exact component={LoginPage} />
-        <Route path="/login/:type" exact component={LoginPage} />
-        <Route path="/app/:id" exact component={BookingApp} />  
-        <Route path="/" exact render={ () => <Redirect to="/login"/> }/>
-        </React.Fragment>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <React.Fragment>
+      <Route path="/app/:id/:isAdmin" exact component={BookingApp} />
+      <Route path="/login" exact component={LoginPage} />
+      <Route path="/login/:type" exact component={LoginPage} />
+      <Route path="/app/:id" exact component={BookingApp} />
+      <Route path="/" exact render={() => <Redirect to="/login" />} />
+    </React.Fragment>
+  </Router>
+);
 
 
 export default App;
