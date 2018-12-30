2510e46a100da6b342f0e631cb691427b191725b
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-30 17:26:51 +0100

    linted requireLogin

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -4,15 +4,14 @@ const requireLogin = (req, res, next) => {
      * checks if user is logged in, if user is loggin in, passport - which
      * runs before requireLogin - should have added a "user" property to req
      */
-    if(!req.user) {
+    if (!req.user) {
       res.locals.type = 'clientErrorUnauthorized';
       throw new Error('You are not authorized to access this page. Please log in first.');
     }
-    next()
-  } catch(err) {
+    next();
+  } catch (err) {
     next(err);
   }
-
-}
+};
 
 module.exports = requireLogin;
