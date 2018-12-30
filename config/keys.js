0efdeb7c1b5c56f68dc08733e050b62da9ded841
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-30 19:15:36 +0100

    require statements need to be inside if satements

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,10 @@
-const devKeys = require('./dev');
-const prodKeys = require('./prod');
-
 // keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === 'production') {
   // we are in production - return the prod set of keys
+  const prodKeys = require('./prod');
   module.exports = prodKeys;
 } else {
   // we are in development - return the dev keys!!!
+  const devKeys = require('./dev');
   module.exports = devKeys;
 }
