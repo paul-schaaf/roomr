a4bd8ff50f520be71c2d5b090010bf9da53c21d7
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-30 18:08:45 +0100

    linted LoginPage

--- client-roomr/src/components/loginPage/LoginPage.js
+++ client-roomr/src/components/loginPage/LoginPage.js
@@ -1,17 +1,21 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import './LoginPage.css';
 import LoginMessage from './LoginMessage';
 
 class LoginPage extends React.Component {
   state = {
-    tab: 'login' ,
+    tab: 'login',
     loginColor: '#1E4363',
     createColor: '#152F45',
     submitValue: 'login',
     entity: '',
     email: '',
     password: '',
-    info: this.props.match.params.type
+    info: this.props.match.params.type,
   };
 
   onLoginTab = () => {
@@ -22,62 +26,62 @@ class LoginPage extends React.Component {
     this.setState({ tab: 'create', loginColor: '#152F45', createColor: '#1E4363' });
   }
 
-  onInputChange = event => {
+  onInputChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
   }
 
-  getFormAction = () => {
-    return (this.state.tab === 'login') ? '/api/login' : '/api/entities';
-  }
+  getFormAction = () => ((this.state.tab === 'login') ? '/api/login' : '/api/entities')
 
   onButtonClick = () => {
     this.setState({ info: '' });
   }
 
-  render () {
+  render() {
     return (
       <div className="login-page">
         <div className="login-box">
-          {this.props.match.params && this.state.info && <LoginMessage onButtonClick={this.onButtonClick}info={this.state.info}/>}
+          {this.props.match.params && this.state.info
+          && <LoginMessage onButtonClick={this.onButtonClick} info={this.state.info} />}
           <div className="login-box__tabs">
-            <div style={{ background: this.state.loginColor }} onClick={this.onLoginTab} >Login</div>
-            <div style={{ background: this.state.createColor }} onClick={this.onCreateTab} >Create</div>
+            <div role="button" style={{ background: this.state.loginColor }} onClick={this.onLoginTab}>Login</div>
+            <div role="button" style={{ background: this.state.createColor }} onClick={this.onCreateTab}>Create</div>
           </div>
           <div className="login-box__form-box">
             <form
-            action={this.getFormAction()}
-            method="POST">
+              action={this.getFormAction()}
+              method="POST"
+            >
               <input
                 name="entity"
                 onChange={this.onInputChange}
                 className="login-box__form-box__input"
                 type="text"
-                placeholder="entity...">
-              </input>
+                placeholder="entity..."
+              />
               <input
                 name="email"
                 onChange={this.onInputChange}
                 className="login-box__form-box__input"
                 type="text"
-                placeholder="email...">
-              </input>
+                placeholder="email..."
+              />
               <input
                 name="password"
                 onChange={this.onInputChange}
                 className="login-box__form-box__input"
                 type="password"
-                placeholder="password...">
-              </input>
+                placeholder="password..."
+              />
               <input
                 className="login-submit"
                 type="submit"
-                value={this.state.tab}>
-              </input>
+                value={this.state.tab}
+              />
             </form>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
