a8b96bada81b5fe85975c3440c252e76342cb47d
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-30 17:26:46 +0100

    linted requireAdmin

--- middlewares/requireAdmin.js
+++ middlewares/requireAdmin.js
@@ -4,15 +4,14 @@ const requireAdmin = (req, res, next) => {
      * checks if user is logged in, if user is loggin in, passport - which
      * runs before requireLogin - should have added a "user" property to req
      */
-    if(!req.user.isAdminNow) {
+    if (!req.user.isAdminNow) {
       res.locals.type = 'clientErrorUnauthorized';
-      throw new Error('You are not authorized to access this page. You are not an admin of this entity.')
+      throw new Error('You are not authorized to access this page. You are not an admin of this entity.');
     }
-    next()
-  } catch(err) {
+    next();
+  } catch (err) {
     next(err);
   }
-
-}
+};
 
 module.exports = requireAdmin;
