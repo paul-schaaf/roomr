23f7f4e1279f634491b8882beb289c3a7cc80d86
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-30 18:15:37 +0100

    linted LoadingPage

--- client-roomr/src/components/bookingApp/specialPages/LoadingPage.js
+++ client-roomr/src/components/bookingApp/specialPages/LoadingPage.js
@@ -1,12 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import './LoadingPage.css';
 
-const LoadingPage = () => {
-  return (
-    <div className="loading-body">
-      <div className="loading-box"></div>
-    </div>
-  )
-}
+const LoadingPage = () => (
+  <div className="loading-body">
+    <div className="loading-box" />
+  </div>
+);
 
 export default LoadingPage;
