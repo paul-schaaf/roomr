12c5f76f81fca5e5b0569d6c804f943eb3773f9f
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-30 18:20:54 +0100

    linted DataList

--- client-roomr/src/components/bookingApp/formArea/DataList.js
+++ client-roomr/src/components/bookingApp/formArea/DataList.js
@@ -1,21 +1,18 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 
 const DataList = ({ data, id, type }) => {
-  if(type === 'room') {
-    const renderedOptions = data.map(roomObject => {
-      return <option key={roomObject.roomName}value={roomObject.roomName}></option>
-    })
-  
+  if (type === 'room') {
+    const renderedOptions = data
+      .map(roomObject => <option key={roomObject.roomName} value={roomObject.roomName} />);
+
     return <datalist id={id}>{renderedOptions}</datalist>;
   }
-  
-  if(type === 'user') {
-    const renderedUsers = data.map((userObject) => {
-      return <option key={userObject} value={userObject}></option>
-    })
 
-    return <datalist id={id}>{renderedUsers}</datalist>;
-  }
-}
+
+  const renderedUsers = data.map(userObject => <option key={userObject} value={userObject} />);
+
+  return <datalist id={id}>{renderedUsers}</datalist>;
+};
 
 export default DataList;
