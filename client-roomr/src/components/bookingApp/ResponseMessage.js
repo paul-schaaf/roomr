56ef661f890e78a9426b287e052f97a7dd892cfa
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-30 18:11:36 +0100

    linted ResponseMessage

--- client-roomr/src/components/bookingApp/ResponseMessage.js
+++ client-roomr/src/components/bookingApp/ResponseMessage.js
@@ -1,3 +1,6 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import './ResponseMessage.css';
 
@@ -5,25 +8,25 @@ import './ResponseMessage.css';
 * this component renders an error message on the top of the screen. more info in App.js
 */
 class ResponseMessage extends React.Component {
-  state =  {
+  state = {
     background: '#1E4363',
     buttonBackground: '#1E4363',
-    buttonColor: 'white'
+    buttonColor: 'white',
   }
 
   componentDidMount = async () => {
     if (this.props.clientError) {
       await this.setState({ background: '#BC2D19' });
-      await this.setState({ buttonBackground: this.state.background });
+      await this.setState(prevState => ({ buttonBackground: prevState.background }));
       this.setState({ buttonColor: 'white' });
     }
   }
 
-  onMessageButtonClick = event => {
+  onMessageButtonClick = () => {
     this.props.onMessageButtonClick();
   }
 
-    /**
+  /**
    * normal css hover isn't possible for this component since
    * its background is either blue or red
    * hence the button's background needs to change dynamically
@@ -31,30 +34,35 @@ class ResponseMessage extends React.Component {
    */
   onIsHovered = () => {
     this.setState({ buttonBackground: 'white' });
-    this.setState({ buttonColor: this.state.background });
+    this.setState(prevState => ({ buttonColor: prevState.background }));
   }
 
   onIsNotHovered = () => {
-    this.setState({ buttonBackground: this.state.background });
-    this.setState({ buttonColor: 'white'});
+    this.setState(prevState => ({ buttonBackground: prevState.background }));
+    this.setState({ buttonColor: 'white' });
   }
 
-  render () {
+  render() {
     return (
       <div style={{ background: this.state.background }} className="response-message">
-        {this.props.responseMessage !== 'loading' && 
-        <p className="response-message__text">{this.props.responseMessage}</p>}
-        {this.props.responseMessage !== 'loading' && <button
-        style={{ background: this.state.buttonBackground, color: this.state.buttonColor }}
-        onMouseEnter={this.onIsHovered}
-        onMouseLeave={this.onIsNotHovered}
-        className="response-message-button"
-        type="button"
-        onClick={this.onMessageButtonClick}>X</button>}
-        {this.props.responseMessage === 'loading' &&
-        <div className="response-message__loading-box"></div>}
+        {this.props.responseMessage !== 'loading'
+        && <p className="response-message__text">{this.props.responseMessage}</p>}
+        {this.props.responseMessage !== 'loading' && (
+        <button
+          style={{ background: this.state.buttonBackground, color: this.state.buttonColor }}
+          onMouseEnter={this.onIsHovered}
+          onMouseLeave={this.onIsNotHovered}
+          className="response-message-button"
+          type="button"
+          onClick={this.onMessageButtonClick}
+        >
+        X
+        </button>
+        )}
+        {this.props.responseMessage === 'loading'
+        && <div className="response-message__loading-box" />}
       </div>
-    )
+    );
   }
 }
 
