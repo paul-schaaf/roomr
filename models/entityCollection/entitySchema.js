8854cef66b213c393352b0aec5bbec4e72c0e3c4
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-30 17:25:35 +0100

    linted entitySchema

--- models/entityCollection/entitySchema.js
+++ models/entityCollection/entitySchema.js
@@ -1,21 +1,22 @@
 const mongoose = require('mongoose');
+
 const { Schema } = mongoose;
 const users = require('./userSubSchema');
 const rooms = require('./roomSubSchema');
 
-const entitySchema = new Schema ({
+const entitySchema = new Schema({
   name: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   adminCount: {
     type: Number,
     required: true,
-    default: 1
+    default: 1,
   },
   users: [users],
-  rooms: [rooms]
+  rooms: [rooms],
 });
 
 mongoose.model('entities', entitySchema);
