6b0a4a74491cbbdafa2a2dffe0c70f1130cb8e5c
Paul Schaaf <paulsimonschaaf@gmail.com>
2018-12-30 18:18:18 +0100

    linted RoomList

--- client-roomr/src/components/bookingApp/infoArea/RoomList.js
+++ client-roomr/src/components/bookingApp/infoArea/RoomList.js
@@ -1,20 +1,20 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import Room from './Room';
 
 /*
-* state.rooms that is passed down into this component as data holds the array of rooms in the database
+* state.rooms that is passed down into this
+* component as data holds the array of rooms in the database
 * we loop over the array to create and render rooms
 */
 
 const RoomList = ({ data }) => {
-  const renderedRooms = data.map((room) => {
-    return <Room key={room.roomName} roomData={room}/>
-  })
+  const renderedRooms = data.map(room => <Room key={room.roomName} roomData={room} />);
   return (
     <React.Fragment>
       {renderedRooms}
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default RoomList;
