f2e9347b808aa658e4229a05804969a7679a0d87
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-04 19:34:58 +0100

    added desctructuring for React.Fragment

--- client-roomr/src/App.js
+++ client-roomr/src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import BookingApp from './components/bookingApp/BookingApp';
 import LoginPage from './components/loginPage/LoginPage';
 
 const App = () => (
   <Router>
-    <React.Fragment>
+    <Fragment>
       <Route path="/app/:id/:isAdmin" exact component={BookingApp} />
       <Route path="/login" exact component={LoginPage} />
       <Route path="/login/:type" exact component={LoginPage} />
       <Route path="/app/:id" exact component={BookingApp} />
       <Route path="/" exact render={() => <Redirect to="/login" />} />
-    </React.Fragment>
+    </Fragment>
   </Router>
 );
 
