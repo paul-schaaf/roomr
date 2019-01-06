7cb0bdff113a5aeaa4857e7834c7719ff165222b
Paul Schaaf <paulsimonschaaf@gmail.com>
2019-01-06 12:02:01 +0100

    corrected pseudocode

--- middlewares/databaseUpdate.js
+++ middlewares/databaseUpdate.js
@@ -4,7 +4,7 @@ const databaseUpdate = (req, res, next) => {
   const currentDay = moment.utc().day();
   const currentHour = moment.utc().hour();
   /**
-   *    if (has there been an update during this sat-fri)
+   *    if (has there been an update during this sat>UTC12-fri)
    *       dont update
    *     else 
    *       update
